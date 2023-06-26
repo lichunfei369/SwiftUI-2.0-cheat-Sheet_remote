b3beafa0e807fc2f13cbe8b931fd52dc4eed5c2c
Apple <lichunfeigood@gmail.com>
2023-06-26 16:14:15 +0800

    Initial Commit

---
+++ SwiftUI-2.0-cheat-Sheet/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-2.0-cheat-Sheet
+//
+//  Created by Apple on 2023/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-2.0-cheat-Sheet/SwiftUI_2_0_cheat_SheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_2_0_cheat_SheetApp.swift
+//  SwiftUI-2.0-cheat-Sheet
+//
+//  Created by Apple on 2023/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_2_0_cheat_SheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
